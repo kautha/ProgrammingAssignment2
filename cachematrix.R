23e474d115e80ddd79cedcc47e040d84e9b2c6e2
Chandrika Kautha <chandu.kautha@gmail.com>
2017-05-06 17:17:48 -0500

    Added code to compute inverse of matrix and cache it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Input argument of makeCacheMatrix is an invertible matrix. This function when 
+##  used along with cacheSolve function, can cache its own matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        # x is an invertible matrix
+        # initializing invert of matrix to NULL
+        inv = NULL
+        # setting matrix value to the value of inputted invertible matrix x
+        # and its inverse to NULL
+        set = function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        # get function returns the matrix x
+        get = function() x
+        # setinverse sets a value to inverse matrix
+        setinverse = function(inverse) inv <<- inverse 
+        # getinverse returns the inverse matrix
+        getinverse = function() inv
+        # function returns list containing functions to set & get a matrix and 
+        # its inverse.
+        list(set=set, get=get, setinv=setinverse, getinv=getinverse)
 }
-
-
-## Write a short comment describing this function
+# The function returns the cached inverse matrix. If it is not found in the 
+# cached variable then it computes it and then returns.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## x is a list of functions to get & set a matrix value and its inverse
+        inv = x$getinv()
+        
+        # if the inverse value is not NULL, function returns the cached value
+        if (!is.null(inv)) return(inv)
+        
+        # if the inverse value is NULL, its value is computed
+        inv = solve(x$get(), ...)
+        
+        # sets the value of the inverse in the cache 
+        x$setinv(inv)
+        
+        return(inv)
 }
